4bcd638335009b07628c1d009de2125a67faea89
zhengyu <zhengyu@hd123.com>
2025-12-17 11:40:01 +0800

    新增hdportal-polarDbOracle输出器

--- printers/hdportal/hdportal_printer.py
+++ printers/hdportal/hdportal_printer.py
@@ -143,6 +143,69 @@ class HdportalOraclePrinter(printer.Printer):
         pass
 
 
+class HdportalPolarDbOPrinter(printer.Printer):
+
+    def __init__(self):
+        pass
+
+    def name(self) -> str:
+        return "hdportal-polarDbOracle实现"
+
+    def print_create_table(self, table: schema.Table):
+        print(f"CALL rb_create_table('{table.name}', '")
+
+        column_holder = print_util.ColumnHolder()
+        for column in table.columns:
+            row_parts: list[str] = [column.name]
+            match column.data_type:
+                case DataType.Type.VARCHAR.name:
+                    row_parts.append(f'VARCHAR({column.length})')
+                case DataType.Type.BIT.name | DataType.Type.BOOLEAN.name:
+                    row_parts.append('BOOLEAN')
+                case DataType.Type.DECIMAL.name:
+                    row_parts.append(f'DECIMAL({print_util.format_decimal(column.length)})')
+                case DataType.Type.DATETIME.name:
+                    row_parts.append('TIMESTAMP')
+                case _:
+                    if column.data_type:
+                        row_parts.append(str(column.data_type))
+                    else:
+                        print('数据类型为空')
+            third_part = []
+            if column.notnull:
+                third_part.append('NOT NULL')
+            if column.default:
+                if isinstance(column.default, str):
+                    third_part.append(f'DEFAULT \'\'{column.default}\'\'')
+                else:
+                    third_part.append(f'DEFAULT {column.default}')
+            if len(third_part) > 0:
+                row_parts.append(' '.join(third_part))
+            column_holder.add_row(row_parts)
+        column_format_sql = column_holder.to_sql()
+
+        if len(table.pks) > 0:
+            column_format_sql += ',\n'
+            column_format_sql += f'  PRIMARY KEY ({', '.join(table.pks[0].columns)})\', null);'
+            pass
+        else:
+            column_format_sql += ', null);'
+        print(column_format_sql)
+
+        # 索引
+        for idx in table.idxes:
+            if idx.unique:
+                print(
+                    f'CALL rb_create_unique_key(\'{table.name}\', \'{idx.name}\', \'{', '.join(idx.columns)}\', \'\');')
+            else:
+                print(
+                    f'CALL rb_create_index(\'{table.name}\', \'{idx.name}\', \'{', '.join(idx.columns)}\', null, null, null);')
+        pass
+
+    def print_function_call(self, command: schema.Command):
+        pass
+
+
 class HdportalJavaPrinter(printer.Printer):
 
     def __init__(self):
@@ -181,6 +244,7 @@ class HdportalJavaPrinter(printer.Printer):
             variable = string_util.to_program_variable(column.name)
             print(f'  private {java_type} {variable};')
         print()
+
     pass
 
     def print_function_call(self, command: schema.Command):
